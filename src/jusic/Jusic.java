c4f73b178664cf62d351a98bdafc33b7ec23eceb
limsm9449 <limsm9449@naver.com>
2021-01-14 17:19:07 +0900

    20200114

--- src/jusic/Jusic.java
+++ src/jusic/Jusic.java
@@ -21,22 +21,24 @@ public class Jusic {
 	private static Connection conn;
 
 	public static void main(String[] args) {
-		System.out.println("Jusic Start");
+		//System.out.println("Jusic Start");
 
 		try {
-			jusic100();
+			jusic100_1();
+			jusic100_2();
+			//upjong();
+			//theme();
 	        
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		
-		System.out.println("Jusic End");
+		//System.out.println("Jusic End");
 	}
 
-	public static void jusic100() throws Exception {
-		String url = "https://finance.naver.com/sise/sise_market_sum.nhn?sosok=0";
+	public static void jusic100_1() throws Exception {
+		String url = "https://finance.naver.com/sise/sise_market_sum.nhn";
 
-		// 동아 entryId를 찾는다.
 		Document doc = Jsoup.connect(url).timeout(10000).get();
 		//System.out.println(doc);
 		Elements eTables = doc.select("table");
@@ -50,6 +52,77 @@ public class Jusic {
 		}
 	}
 
+	public static void jusic100_2() throws Exception {
+		String url = "https://finance.naver.com/sise/sise_market_sum.nhn?&page=2";
+
+		Document doc = Jsoup.connect(url).timeout(10000).get();
+		//System.out.println(doc);
+		Elements eTables = doc.select("table");
+		for (Element e_eTables : eTables) {
+			if ("type_2".equals(e_eTables.attr("class"))) {
+				Elements tTds = e_eTables.select("tbody tr td .tltle");
+				for (Element e_tTds : tTds) {
+					System.out.println("tJusics[idx++] = lfn_createJusicInfo('" + e_tTds.text() + "', '" + getUrlParameter(e_tTds.attr("href"), "code") + "', 0, 0);");
+				}
+			}
+		}
+	}
+
+	public static void upjong() throws Exception {
+		String url = "https://finance.naver.com/sise/sise_group.nhn?type=upjong";
+
+		Document doc = Jsoup.connect(url).timeout(10000).get();
+		//System.out.println(doc);
+		Elements eAs = doc.select("table tr td a");
+		for (Element e_eAs : eAs) {
+			if ( getUrlParameter(e_eAs.attr("href"), "no") != "" ) {
+				//System.out.println("<option value='" + getUrlParameter(e_eAs.attr("href"), "no") + "'>" + e_eAs.text() + "</option>");
+				upjongDetail(getUrlParameter(e_eAs.attr("href"), "no"));
+			}
+		}
+	}
+
+	public static void upjongDetail(String no) throws Exception {
+		String url = "https://finance.naver.com/sise/sise_group_detail.nhn?type=upjong&no=" + no;
+
+		System.out.println("idx = 0;");
+		System.out.println("jusicArr['upjong_" + no + "'] = []");
+		
+		Document doc = Jsoup.connect(url).timeout(10000).get();
+		//System.out.println(doc);
+		Elements eAs = doc.select(".type_5 tbody tr td .name_area a");
+		for (Element e_eAs : eAs) {
+			System.out.println("jusicArr['upjong_" + no + "'][idx++] = lfn_createJusicInfo('" + e_eAs.text() + "', '" + getUrlParameter(e_eAs.attr("href"), "code") + "', 0, 0);");
+		}
+	}
+
+	public static void theme() throws Exception {
+		String url = "https://finance.naver.com/sise/sise_group.nhn?type=theme";
+
+		Document doc = Jsoup.connect(url).timeout(10000).get();
+		//System.out.println(doc);
+		Elements eAs = doc.select("table tr td a");
+		for (Element e_eAs : eAs) {
+			if ( getUrlParameter(e_eAs.attr("href"), "no") != "" ) {
+				//System.out.println("<option value='" + getUrlParameter(e_eAs.attr("href"), "no") + "'>" + e_eAs.text() + "</option>");
+				themeDetail(getUrlParameter(e_eAs.attr("href"), "no"));
+			}
+		}
+	}
+
+	public static void themeDetail(String no) throws Exception {
+		String url = "https://finance.naver.com/sise/sise_group_detail.nhn?type=theme&no=" + no;
+
+		System.out.println("idx = 0;");
+		System.out.println("jusicArr['theme_" + no + "'] = []");
+		
+		Document doc = Jsoup.connect(url).timeout(10000).get();
+		//System.out.println(doc);
+		Elements eAs = doc.select(".type_5 tbody tr td .name_area a");
+		for (Element e_eAs : eAs) {
+			System.out.println("jusicArr['theme_" + no + "'][idx++] = lfn_createJusicInfo('" + e_eAs.text() + "', '" + getUrlParameter(e_eAs.attr("href"), "code") + "', 0, 0);");
+		}
+	}
 
 	public static String getUrlParameter(String url, String parameter) throws Exception {
 		String rtn = "";
